fb5bfc8a1a5b35f04c27c124934e01ddb0cf1fef
Aaron Segura <aaron@aaronsegura.com>
2025-03-17 06:32:34 -0500

    Run coverage once

--- .vscode/run_coverage.sh
+++ .vscode/run_coverage.sh
@@ -1,3 +1,3 @@
-# coverage run --source nextcloud_async -m pytest
-# coverage lcov -o ./lcov.info
-pytest --block-network --continue-on-collection-errors --cov=nextcloud_async --cov-report lcov:lcov.info tests/
+coverage run --source nextcloud_async -m pytest tests/unit --block-network
+coverage lcov -o ./lcov.info
+# pytest --block-network -cov=nextcloud_async --cov-report lcov:lcov.info tests
