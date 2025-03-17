10a9a49ba9d8d4f9b425c57ebefd4b03007b7531
Aaron Segura <aaron@aaronsegura.com>
2025-03-17 06:30:56 -0500

    Add APP_TOKEN from environment

--- tests/constants.py
+++ tests/constants.py
@@ -7,6 +7,7 @@ NEXTCLOUD_VERSION = os.environ.get("PYTEST_NEXTCLOUD_VERSION", "30")
 USER = os.environ.get("PYTEST_NEXTCLOUD_USER", "admin")
 PASSWORD = os.environ.get("PYTEST_NEXTCLOUD_PASSWORD", "admin")
 ENDPOINT = os.environ.get("PYTEST_NEXTCLOUD_ENDPOINT", "http://localhost:8181")
+APP_TOKEN = os.environ.get("PYTEST_NEXTCLOUD_APP_TOKEN", "[app token]")
 
 REMOTE_TEST_DIR = "/.nextcloud-async-pytest"
 USER_AGENT = f"nextcloud-async-pytest/{VERSION}"
