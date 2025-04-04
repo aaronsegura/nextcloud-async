cfc64fb747c56f7ce242eb3ab04cfb4f0f171716
Aaron Segura <aaron@aaronsegura.com>
2025-04-04 06:07:45 -0500

    test_users Rename fixtures

--- tests/integration/test_groups.py
+++ tests/integration/test_groups.py
@@ -6,7 +6,7 @@ from collections.abc import AsyncGenerator
 from nextcloud_async.api import Group, GroupsApi, User, UsersApi
 
 _TEST_GROUP_NAME = "pytest_group"
-_TEST_USER = {
+_nc_user = {
     "user_id": "pytest_user",
     "display_name": "Pytest User Guy",
     "email": "pytest@example.com",
@@ -17,7 +17,7 @@ _TEST_USER = {
 
 
 @pytest_asyncio.fixture(scope="function", loop_scope="session")
-async def test_groups(groups_api: GroupsApi) -> AsyncGenerator[list[Group], None]:
+async def nc_groups(groups_api: GroupsApi) -> AsyncGenerator[list[Group], None]:
     ret: list[Group] = []
 
     for i in range(0, 2):
@@ -32,42 +32,40 @@ async def test_groups(groups_api: GroupsApi) -> AsyncGenerator[list[Group], None
 
 
 @pytest_asyncio.fixture(scope="function", loop_scope="session")
-async def test_user(users_api: UsersApi) -> AsyncGenerator[User]:
-    test_user = await users_api.create(**_TEST_USER)
-    yield test_user
-    await test_user.delete()
+async def nc_user(users_api: UsersApi) -> AsyncGenerator[User]:
+    nc_user = await users_api.create(**_nc_user)
+    yield nc_user
+    await nc_user.delete()
 
 
 @pytest.mark.integration
 @pytest.mark.vcr
 @pytest.mark.asyncio(loop_scope="session")
 class TestGroups:
-    async def test_search_groups(self, groups_api: GroupsApi, test_groups: list[Group]):
-        group = test_groups[0]
+    async def test_search_groups(self, groups_api: GroupsApi, nc_groups: list[Group]):
+        group = nc_groups[0]
         groups = await groups_api.search(group.id)
         assert len(groups) == 1
 
-    async def test_create_group(self, test_groups: list[Group]):
-        group = test_groups[0]
+    async def test_create_group(self, nc_groups: list[Group]):
+        group = nc_groups[0]
         assert isinstance(group, Group)
         assert group.id == f"{_TEST_GROUP_NAME}_0"
 
-    async def test_set_get_group_members(self, test_groups: list[Group], test_user: User):
-        group = test_groups[0]
-        await test_user.add_to_group(group)
+    async def test_set_get_group_members(self, nc_groups: list[Group], nc_user: User):
+        group = nc_groups[0]
+        await nc_user.add_to_group(group)
         members = await group.get_members()
-        assert test_user.id in members
+        assert nc_user.id in members
 
-    async def test_set_get_group_subadmins(
-        self, test_groups: list[Group], test_user: User
-    ):
-        group = test_groups[0]
-        await test_user.promote_to_group_subadmin(group)
+    async def test_set_get_group_subadmins(self, nc_groups: list[Group], nc_user: User):
+        group = nc_groups[0]
+        await nc_user.promote_to_group_subadmin(group)
         subadmins = await group.get_subadmins()
-        assert test_user.id in subadmins
+        assert nc_user.id in subadmins
 
-    async def test_remove_group(self, groups_api: GroupsApi, test_groups: list[Group]):
-        group = test_groups[1]
+    async def test_remove_group(self, groups_api: GroupsApi, nc_groups: list[Group]):
+        group = nc_groups[1]
         id = group.id
         await group.delete()
         response = await groups_api.search(id)
